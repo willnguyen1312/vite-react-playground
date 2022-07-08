7eca51320a38572c84e3cc2002b10e56e6a18f85
Nam Nguyen <nam.nguyen.node@gmail.com>
2022-07-08 16:39:17 +0700

    xD

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -2,7 +2,10 @@ import React from "react";
 
 export default function Counter() {
   const [counter, setCounter] = React.useState(0);
-
+  const [counter2, setCounter2] = React.useState(0);
+  const [counter3, setCounter3] = React.useState(0);
+  console.log("Log me");
+  
   return (
     <div>
       <h1>{counter}</h1>
@@ -12,8 +15,8 @@ export default function Counter() {
           // setCounter(counter => counter + 10);
           // setCounter(counter => counter + 10);
           setCounter(counter + 10);
-          setCounter(counter + 10);
-          setCounter(counter + 10);
+          setCounter2(counter2 + 10);
+          setCounter3(counter3 + 10);
         }}
       >
         Increment
